67168fc03a6fa101ed8ee3e6d4d457da95b80c35
nahucham18 <nahu.chamo19@gmail.com>
2023-04-04 21:00:44 -0300

    Primeros componentes para el proyecto

---
+++ todolist/src/views/Home/Home.jsx
@@ -0,0 +1,14 @@
+import style from './Home.module.css'
+
+import ListaDeTareas from '../ListaDeTareas/ListaDeTareas'
+
+const Home = () =>{
+    return(
+        <div>
+            
+            <ListaDeTareas/>
+        </div>
+    )
+}
+
+export default Home
---
+++ todolist/src/views/ListaDeTareas/ListaDeTareas.jsx
@@ -0,0 +1,65 @@
+import style from './ListaDeTareas.module.css'
+
+const ListaDeTareas = () =>{
+    return (
+        <div>
+            <div className={style.listaLimpieza}>
+                <span>Limpieza</span> <br />
+                <input type="checkbox" />Papel higienico <br />
+                <input type="checkbox" />Dentrifico <br />
+                <input type="checkbox" />Papel de cocina <br />
+                <input type="checkbox" />Detergente <br />
+                <input type="checkbox" />Lavandina  <br />
+                <input type="checkbox" />Desodorante de Ambiente <br />
+            </div>
+
+            <div className={style.listaLimpieza}>
+                <span>Comida</span> <br />
+                <input type="checkbox" />Carne <br />
+                <input type="checkbox" />Pimienta <br />
+                <input type="checkbox" />Mayonesa <br />
+                <input type="checkbox" />Queso cremoso <br />
+                <input type="checkbox" />Crema <br />
+                <input type="checkbox" />Manteca <br />
+                <input type="checkbox" />Morron <br />
+                <input type="checkbox" />Zanahoria <br />
+                <input type="checkbox" />Galletitas Express <br />
+                <input type="checkbox" />Galletita de gallo <br />
+                <input type="checkbox" />Tomate <br />
+                <input type="checkbox" />Exquisita <br />
+                <input type="checkbox" />Dulce de Leche <br />
+                <input type="checkbox" />Tostadas <br />
+                <input type="checkbox" />Queso rallado <br />
+                <input type="checkbox" />Atun <br />
+                <input type="checkbox" />Arroz <br />
+                <input type="checkbox" />Harina <br />
+                <input type="checkbox" />Leche <br />
+                <input type="checkbox" />Ajo <br />
+                <input type="checkbox" />Lechuga <br />
+                <input type="checkbox" />Leche <br />
+            </div>
+
+            <div className={style.listaLimpieza}>
+                <span>Limpieza</span> <br />
+                <input type="checkbox" />Papel higienico <br />
+                <input type="checkbox" />Dentrifico <br />
+                <input type="checkbox" />Papel de cocina <br />
+                <input type="checkbox" />Detergente <br />
+                <input type="checkbox" />Lavandina  <br />
+                <input type="checkbox" />Desodorante de Ambiente <br />
+            </div>
+
+            <div className={style.listaLimpieza}>
+                <span>Limpieza</span> <br />
+                <input type="checkbox" />Papel higienico <br />
+                <input type="checkbox" />Dentrifico <br />
+                <input type="checkbox" />Papel de cocina <br />
+                <input type="checkbox" />Detergente <br />
+                <input type="checkbox" />Lavandina  <br />
+                <input type="checkbox" />Desodorante de Ambiente <br />
+            </div>
+        </div>
+    )
+}
+
+export default ListaDeTareas
